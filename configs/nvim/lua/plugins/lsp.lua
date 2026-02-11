71a90eef1e1afc68a7adc86ac8a40f74a69634c4
MLC Bloeiman <mar@strawmelonjuice.com>
2026-02-11 13:51:07 +0100

    Gleam didn't fmt?

--- configs/nvim/lua/plugins/lsp.lua
+++ configs/nvim/lua/plugins/lsp.lua
@@ -16,6 +16,7 @@ return {
                     fish = { "fish_indent" },
                     sh = { "shfmt" },
                     nix = { "nixfmt" },
+                    gleam = { "gleam" },
                 },
                 -- The options you set here will be merged with the builtin formatters.
                 -- You can also define any custom formatters here.
