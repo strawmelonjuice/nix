e485a2b20908050f39367bce69220ef7dfbd30c9
MLC Bloeiman <mar@strawmelonjuice.com>
2026-02-14 11:26:57 +0100

    GRR d*scord

--- home/modules/conversation.nix
+++ home/modules/conversation.nix
@@ -4,5 +4,8 @@
   home.packages = with pkgs; [
     beeper
     fluffychat
+    # Not agreeing with installing...
+    discord
+    # But alas, as beeper is being a bad girl and I'm not bothered to fix it.
   ];
 }
