d8d2e415c27b6f7173ed1c1c8f6c8cf86a783033
MLC Bloeiman <mar@strawmelonjuice.com>
2026-02-16 23:23:55 +0100

    now that im busy

--- configs/fish/config/20-aliases.fish
+++ configs/fish/config/20-aliases.fish
@@ -85,7 +85,7 @@ function zap
 
         # Dynamically tell user of flakes.
         if test -f flake.nix; and type -q nix; and not set -q IN_NIX_SHELL; and not test -f .envrc
-            echo -e "❄️\tFound flake.nix, run 'create-envrc' to create a .envrc file here to load this flake automatically."
+            echo -e "\n\n❄️\tFound flake.nix, run 'create-envrc' to create a .envrc file here to load this flake automatically."
             echo -e "\tOr alternatively, run 'dev' to open Fish in a nix develop shell."
         end
     else
