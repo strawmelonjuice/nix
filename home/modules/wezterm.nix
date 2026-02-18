35d209a5fa765bed65d5b92881ba345676e476a2
MLC Bloeiman <mar@strawmelonjuice.com>
2026-02-18 21:47:16 +0100

    make opaque

--- home/modules/wezterm.nix
+++ home/modules/wezterm.nix
@@ -24,7 +24,7 @@
        local config = wezterm.config_builder()
        config.color_scheme = get_time_of_day()
        config.enable_tab_bar = false
-       config.window_background_opacity = 0.95
+       config.window_background_opacity = 1.0
        config.font = wezterm.font('Maple Mono NF')
        config.window_padding = {
           left = 1,
