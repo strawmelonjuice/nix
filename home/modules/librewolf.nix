fcab49356abdfaff04f3307cbbc84c4e0b9dd193
MLC Bloeiman <mar@strawmelonjuice.com>
2026-02-27 13:10:07 +0100

    Re-enable librewolf now that it's cached and I won't have to compile from source!

--- home/modules/librewolf.nix
+++ home/modules/librewolf.nix
@@ -1,16 +1,16 @@
 { pkgs, ... }:
 
 {
-  # programs.librewolf = {
-    # enable = true;
-    # settings = {
-      # "privacy.clearOnShutdown.history" = false;
-      # "privacy.clearOnShutdown.downloads" = false;
-      # "privacy.clearOnShutdown.cookies" = false;
-      # "middlemouse.paste" = false;
-      # "general.autoscroll" = true;
-      # "privacy.resistFingerprinting" = false;
-      # "privacy.resistFingerprinting.autoDeclineNoUserInputCanvasPrompts" = true;
-    # };
-  # };
+  programs.librewolf = {
+    enable = true;
+    settings = {
+      "privacy.clearOnShutdown.history" = false;
+      "privacy.clearOnShutdown.downloads" = false;
+      "privacy.clearOnShutdown.cookies" = false;
+      "middlemouse.paste" = false;
+      "general.autoscroll" = true;
+      "privacy.resistFingerprinting" = false;
+      "privacy.resistFingerprinting.autoDeclineNoUserInputCanvasPrompts" = true;
+    };
+  };
 }
