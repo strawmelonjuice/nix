9ce96bcdc322415a5ff63a218863c3f8035cc516
MLC Bloeiman <mar@strawmelonjuice.com>
2026-03-14 08:25:54 +0100

    lix

--- hosts/all-hosts.nix
+++ hosts/all-hosts.nix
@@ -6,6 +6,19 @@
 }:
 {
 
+  nixpkgs.overlays = [
+    (final: prev: {
+      inherit (prev.lixPackageSets.stable)
+        nixpkgs-review
+        nix-eval-jobs
+        nix-fast-build
+        colmena
+        ;
+    })
+  ];
+
+  nix.package = pkgs.lixPackageSets.stable.lix;
+
   nixpkgs.config = {
     permittedInsecurePackages = [
     ];
