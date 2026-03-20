2a5b091839fcc97c7910fcacd0881022f63addc1
MLC Bloeiman <mar@strawmelonjuice.com>
2026-03-20 13:06:58 +0100

    Mise and just out of system installs.
    Signed-off-by: MLC Bloeiman <mar@strawmelonjuice.com>

--- home/modules/development.nix
+++ home/modules/development.nix
@@ -8,13 +8,13 @@
     ./development/vscodium.nix
   ];
   programs.mise = {
-    enable = true;
+    enable = false;
     enableZshIntegration = false;
     enableFishIntegration = false;
   };
   home.packages = with pkgs; [
     # Mise is being faded out in favor of just due to growing friction between me and Mise's style
-    just
+    # just # Hope I get away with this, having flakes and all.
     just-lsp
     # I make a lot of things with Inkscape actually!
     inkscape
