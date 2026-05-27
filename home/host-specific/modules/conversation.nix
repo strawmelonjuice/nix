0fbede17d84b36dcb61d61fb4527f3d9c0f4837f
MLC Bloeiman <mar@strawmelonjuice.com>
2026-05-27 17:01:15 +0200

    rebridged!
    Signed-off-by: MLC Bloeiman <mar@strawmelonjuice.com>

--- home/host-specific/modules/conversation.nix
+++ home/host-specific/modules/conversation.nix
@@ -2,8 +2,6 @@
 
 {
   home.packages = with pkgs; [
-    # cinny-desktop <- Seems letting Vivaldi create a PWA everytime works better, can't copy/paste and stuff
-    # ... unbridged signal again after the people decided I should
-    signal-desktop
+    # cinny-desktop <- Seems browser works better, can't copy/paste and stuff
   ];
 }
